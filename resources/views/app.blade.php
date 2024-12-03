559d8f8c969637b891266ececaafa9b6ceba41fe
alif <alifadiawan2005@gmail.com>
2024-12-03 14:17:02 +0700

    install vue and deedencies

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Planify</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css"
+        integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @inertiaHead
+    @routes
+</head>
+
+<body class="bg-base-100 text-white">
+    @inertia
+</body>
+<script>
+    window.AuthUser = @json(Auth::user());
+</script>
+</html>
