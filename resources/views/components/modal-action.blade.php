fbf05d2cea38da8ac90d45d87a882481236d0317
feliciaadeliaa <feliciaa2811@gmail.com>
2024-12-05 23:25:08 +0700

    progress lagi weekly planner

--- resources/views/components/modal-action.blade.php
+++ resources/views/components/modal-action.blade.php
@@ -17,22 +17,7 @@
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="submit" class="btn btn-primary">Save changes</button>
       </div>
-    </div>
-    <div class="col-12">
-      <div class="mb-3">
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="category" id="category-success" value="success">
-          <label class="form-check-label" for="category-success">Success</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="category" id="category-danger" value="danger">
-          <label class="form-check-label" for="category-danger">Danger</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="category" id="category-warning" value="warning">
-          <label class="form-check-label" for="category-warning">Warning</label>
-        </div>
-      </div>
+     </div>
     </div>
    </form>
   </div>
--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf_token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
     <title>Document</title>
@@ -28,6 +29,7 @@
     <script>
 
         const modal = $('#modal-action');
+        const scrfToken = $('meta[name=csrf_token]').attr('content')
 
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
@@ -36,8 +38,7 @@
          themeSystem: 'bootstrap5',
          events:  `{{ route('events.list') }}`,
          editable: 'true',
-         dateClick: function(info) {
-            console.log(info);
+         dateClick: function(info) { 
        
          $.ajax({
             url: `{{ route('events.create') }}`,
@@ -66,14 +67,60 @@
                 })
             }
        })
-    //    Gunakan Bootstrap 5 modal API tanpa jQuery
-    //    var modal = new bootstrap.Modal(document.getElementById('modal-action'));
-    //    modal.show();
-        $('#modal-action').modal('show')
-             }
-     });
+    },
+        eventClick: function (event){
+            $.ajax({
+            url: `{{ url('events') }}/${event.id}/edit`,
+            success: function (res){
+                modal.html(res).modal('show')
+
+                $('#form-action').on('submit', function(e){
+                    e.preventDefault()
+                    const form = this
+                    const formData = new FormData(form)
+                    $.ajax({
+                        url: form.action,
+                        method: form.method,
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        succes: function (res) {
+                            modal.modal('hide')
+                            calender.refretchEvents()
+                        }
+                    })
+                })
+            }
+       })
+    },
+    eventDrop: function (event) {
+        console.log(event);
+        
+        $.ajax({
+            url: `{{ url('events') }}/${event.id}`,
+            method: 'put',
+            data: {
+                id: event.id,
+                start_date: event.startStr,
+                end_date: event.end.toISOString().substring('0, 10')
+                title: event.title,
+                category: event.extendedProps.category
+            }
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+                accept: 'application/json'
+            },
+            success: function (res){
+                modal.modal('hide')
+
+            }
+       })
+    }
+
+
+    });
         calendar.render();
-     });
+    });
 
 
     //   document.addEventListener('DOMContentLoaded', function() {
